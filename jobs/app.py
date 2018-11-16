0683f6b12cf8ef2cbd12db6c38abd1e7c8e1bdce
Brenden Morris <brendenmorris@mail.weber.edu>
2018-11-16 15:54:46 -0500

    Completed module 4 fixes

--- jobs/app.py
+++ jobs/app.py
@@ -12,16 +12,16 @@ def open_connection():
     connection.row_factory = sqlite3.Row
     return connection
 
-    def execute_sql(sql, values=(), commit=False, single=False):
-        connection = open_connection()
-        cursor = connection.execute(sql, values)
-        if commit == True:
-            results = connection.commit()
-        else:
-            results = cursor.fetchone() if single else cursor.fetchall()
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
 
-        cursor.close()
-        return results
+    cursor.close()
+    return results
 
 @app.teardown_appcontext
 def close_connection(exception):
